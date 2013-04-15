AxiaoJJ/Fuzzing-SA

commit 78fad79d46d838dbc7f13c3ecd86076501b58aa2

hw: move ARM CPU cores to hw/cpu/, configure with default-configs/
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

default-configs/arm-softmmu.mak
@@ -34,6 +34,10 @@ CONFIG_PFLASH_CFI02=y
 CONFIG_MICRODRIVE=y
 CONFIG_USB_MUSB=y
 
+CONFIG_ARM5MPCORE=y
+CONFIG_ARM9MPCORE=y
+CONFIG_ARM15MPCORE=y
+
 CONFIG_ARM_GIC=y
 CONFIG_ARM_GIC_KVM=$(CONFIG_KVM)
 CONFIG_ARM_TIMER=y

hw/arm/Makefile.objs
@@ -1,5 +1,3 @@
-obj-y += arm11mpcore.o a9mpcore.o
-obj-y += a15mpcore.o
 obj-y += strongarm.o
 
 obj-y := $(addprefix ../,$(obj-y))

hw/cpu/Makefile.objs
@@ -1,0 +1,4 @@
+obj-$(CONFIG_ARM11MPCORE) += arm11mpcore.o
+obj-$(CONFIG_ARM9MPCORE) += a9mpcore.o
+obj-$(CONFIG_ARM15MPCORE) += a15mpcore.o
+

commit 7a020d2efdd96607b3f18dcb63a111acaaa2b658

hw: Add lost ARM core again
Commit 78fad79d46d838dbc7f13c3ecd86076501b58aa2 accidentally removed
the compilation of arm11mpcore.c.

Signed-off-by: Stefan Weil <sw@weilnetz.de>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

default-configs/arm-softmmu.mak
@@ -34,8 +34,8 @@ CONFIG_PFLASH_CFI02=y
 CONFIG_MICRODRIVE=y
 CONFIG_USB_MUSB=y
 
-CONFIG_ARM5MPCORE=y
 CONFIG_ARM9MPCORE=y
+CONFIG_ARM11MPCORE=y
 CONFIG_ARM15MPCORE=y
 
 CONFIG_ARM_GIC=y